490eb7b6c803cb1f5c0d8bfa478f66e3c8a573d9
N-nisi <19ib020e@rikkyo.ac.jp>
2021-06-29 18:07:09 +0900

    カウンターを学習

---
+++ 02_レシピ/01_3の倍数/main.js
@@ -0,0 +1,47 @@
+// 引数 number を受ける関数
+// const genkiFunction = function(number) {
+//   // n を 1 から、 number まで、値を1ずつ増やしながら、for の {} の中身を繰り返す
+//   for (let n = 1; n <= number; n++) {
+//     if (n % 3 === 0) {
+//       console.log(n + "!!!!!!!")
+//     } else {
+//       console.log(n)
+//     }
+//   }
+// }
+// genkiFunction(100)
+
+//
+// FizzBuzz
+
+// const FizzBuzzFunction = function(number) {
+//   for (let n = 1; n <= number; n++) {
+//     if (n % 5 === 0 && n % 3 === 0) {
+//       console.log("FizzBuzz")
+//     } else if (n % 3 === 0) {
+//       console.log("Fizz")
+//     } else if (n % 5 === 0) {
+//       console.log("Buzz")
+//     } else {
+//       console.log(n)
+//     }
+//   }
+// }
+// FizzBuzzFunction(100)
+
+//
+// 3が好き
+
+const genkiFunction = function(number) {
+  // n を 1 から、 number まで、値を1ずつ増やしながら、for の {} の中身を繰り返す
+  for (let n = 1; n <= number; n++) {
+    if (n / 10 <= 19 === 3 && n % 10 <= 9) {
+      console.log(n + "!!!!!!!")
+    } else if (n % 10 === 3) {
+      console.log(n + "!!!!!!!!")
+    } else {
+      console.log(n)
+    }
+  }
+}
+genkiFunction(100)
---
+++ 02_レシピ/01_カウンター/main.js
@@ -0,0 +1,24 @@
+const display = document.getElementById("display")
+const plusButton = document.getElementById("plus-button")
+const minusButton = document.getElementById("minus-button")
+
+let count = 0
+
+// // ボタンをクリックしたときの処理を登録
+// plusButton.onclick = function() {
+//   // ここにクリック後の処理を書く
+//   // countを更新
+//   count += 1
+//   // countを表示
+//   display.textContent = count
+// }
+
+// 便利なボタン
+plusButton.onclick = function() {
+  count += 1
+  display.textContent = count
+}
+minusButton.onclick = function() {
+  count -= 1
+  display.textContent = count
+}
