25531f79797004366e85ac7c6b60e25eb3c79333
N-nisi <19ib020e@rikkyo.ac.jp>
2021-07-09 16:35:30 +0900

    [DogAPIで非同期処理]を学習しました。

---
+++ 02_レシピ/02_DogAPIで非同期処理/main.js
@@ -0,0 +1,19 @@
+const imageElement = document.getElementById("dog-image")
+
+// 指定したサーバーにデータを取りに行く
+fetch("https://dog.ceo/api/breeds/image/random")
+  .then((res) => {
+    return res.json() // 結果を json として読み込んで、次の then に渡す
+  })
+  .then((data) => {
+    imageElement.src = data.message // 画像を表示する
+  })
+
+// function(){}の場合 thisの振る舞いという点でアロー関数の方が適している。
+//   fetch("https://dog.ceo/api/breeds/image/random")
+//   .then(function(res) {
+//     return res.json()
+//   })
+//   .then(function(data) {
+//     imageElement.src = data.message
+//   })
